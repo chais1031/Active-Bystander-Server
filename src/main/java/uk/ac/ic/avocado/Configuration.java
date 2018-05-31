706ac40c14cf60de5dd4306e276668d4b9b9e5fa
Nik Vangerow <n@vangerow.uk>
2018-05-31 13:00:20 +0100

    create test webserver with environment awareness

---
+++ src/main/java/uk/ac/ic/avocado/Configuration.java
@@ -0,0 +1,36 @@
+package uk.ac.ic.avocado;
+
+public class Configuration {
+    private static Configuration instance = null;
+    private Type current;
+
+    private Configuration() {
+        switch (System.getenv("BUILD_ENV")) {
+            case "STAGING":
+                current = Type.STAGING;
+                break;
+            case "PRODUCTION":
+                current = Type.PRODUCTION;
+                break;
+            default:
+                throw new RuntimeException("Please set BUILD_ENV to either STAGING or PRODUCTION prior to use!");
+        }
+    }
+
+    public static Configuration getInstance() {
+        if (instance == null) {
+            instance = new Configuration();
+        }
+
+        return instance;
+    }
+
+    public Type getCurrent() {
+        return current;
+    }
+
+    public enum Type {
+        STAGING,
+        PRODUCTION
+    }
+}
--- src/main/java/uk/ac/ic/avocado/Main.java
+++ src/main/java/uk/ac/ic/avocado/Main.java
@@ -1,8 +1,26 @@
 package uk.ac.ic.avocado;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        while (true) {}
+    public static void main(String[] args) throws IOException {
+        final Configuration.Type environment = Configuration.getInstance().getCurrent();
+        final HttpServer server = HttpServer.create(new InetSocketAddress(environment == Configuration.Type.PRODUCTION ? 8080 : 8081), 0);
+        server.createContext("/test", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                final String message = String.format("This is a test originating from the %s environment.", environment.toString());
+                exchange.sendResponseHeaders(200, message.length());
+                exchange.getResponseBody().write(message.getBytes());
+                exchange.close();
+            }
+        });
+        server.setExecutor(null);
+        server.start();
     }
 }
