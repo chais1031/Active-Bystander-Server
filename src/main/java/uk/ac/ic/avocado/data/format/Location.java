8117afb2d2668f83305188e9462e277553b6f207
Alexis Marinoiu <alexis.marinoiu16@imperial.ac.uk>
2018-06-01 00:21:38 +0100

    made Location a bean class; post and get requests are handled properly

--- src/main/java/uk/ac/ic/avocado/data/format/Location.java
+++ src/main/java/uk/ac/ic/avocado/data/format/Location.java
@@ -2,13 +2,28 @@ package uk.ac.ic.avocado.data.format;
 
 public class Location {
 
-  private final Double latitude;
-  private final Double longitude;
-  private final String username;
+  private Double latitude;
+  private Double longitude;
+  private String username;
+
+  public Location() {
+  }
 
   public Location(Double latitude, Double longitude, String username) {
     this.latitude = latitude;
     this.longitude = longitude;
+    this.username = username;
+  }
+
+  public void setLatitude(Double latitude) {
+    this.latitude = latitude;
+  }
+
+  public void setLongitude(Double longitude) {
+    this.longitude = longitude;
+  }
+
+  public void setUsername(String username) {
     this.username = username;
   }
 
