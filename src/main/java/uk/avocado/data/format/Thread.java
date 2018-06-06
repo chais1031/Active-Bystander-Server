b937aa18c7681e99ed2817c53b358e1e2d892b14
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-06 15:58:25 +0100

    setting the title of thread data format

--- src/main/java/uk/avocado/data/format/Thread.java
+++ src/main/java/uk/avocado/data/format/Thread.java
@@ -1,17 +1,31 @@
 package uk.avocado.data.format;
 
+import uk.avocado.Main;
 import uk.avocado.model.Status;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Thread {
 
   private final String threadId;
   private final Status status;
+  private String title = "Anonymous";
 
-  public Thread(uk.avocado.model.Thread thread) {
+  public Thread(uk.avocado.model.Thread thread, String username) {
     this.threadId = thread.getThreadId();
     this.status = thread.getStatus();
+
+    if (status == Status.ACCEPTED) {
+      List<Participant> participants = Main.databaseManager.getParticipantsForThread(threadId);
+
+      title = participants.stream().filter(p -> !p.getUsername().equals(username))
+                                   .map(Participant::getUsername)
+                                   .collect(Collectors.joining(","));
+    }
+
   }
 
   public String getThreadId() {
