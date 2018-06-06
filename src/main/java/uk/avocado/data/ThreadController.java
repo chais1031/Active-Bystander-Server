3b614a772ba37fb8b2fb67867088d789c9081bf9
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-06 12:10:24 +0100

    pass in the username using the request instead

--- src/main/java/uk/avocado/data/ThreadController.java
+++ src/main/java/uk/avocado/data/ThreadController.java
@@ -5,20 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uk.avocado.AvocadoHttpServletRequest;
 import uk.avocado.Main;
 import uk.avocado.data.format.Message;
 import uk.avocado.data.format.Participant;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/thread")
 public class ThreadController {
 
-  //search by username, return all the thread
   @RequestMapping(value = "/participant", method = RequestMethod.GET)
-  public ResponseEntity<List<Participant>> getAllParticipants(@RequestParam(value = "username") String username) {
-    return ResponseEntity.ok(Main.databaseManager.getAllParticipants(username));
+  public ResponseEntity<List<Participant>> getAllParticipants(HttpServletRequest givenRequest) {
+    final AvocadoHttpServletRequest request = new AvocadoHttpServletRequest(givenRequest);
+    return ResponseEntity.ok(Main.databaseManager.getAllParticipants(request.getUsername()));
   }
 
   @RequestMapping(value = "/message", method = RequestMethod.GET)
