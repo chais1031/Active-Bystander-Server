6688ccc7b8b1c0f543f3d7c99002cea0e8265ab2
Alexis Marinoiu <alexis.marinoiu16@imperial.ac.uk>
2018-06-07 14:00:16 +0100

    some more db manager and transaction block tests

--- src/test/java/uk/avocado/database/DatabaseManagerTest.java
+++ src/test/java/uk/avocado/database/DatabaseManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Collections;
 import java.util.List;
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,7 +15,9 @@ public class DatabaseManagerTest {
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
-//  final DatabaseSessionFactory sessionFactory = context.mock(DatabaseSessionFactory.class);
+  private final DatabaseSessionFactory sessionFactory = context.mock(DatabaseSessionFactory.class);
+  private final DatabaseSession session = context.mock(DatabaseSession.class);
+  private final DatabaseTransaction transaction = context.mock(DatabaseTransaction.class);
 
   @Test
   public void callingCreateQueryQueriesDatabase() {
@@ -40,6 +43,7 @@ public class DatabaseManagerTest {
             public List<R> list() {
               return (List<R>) Collections.singletonList(new User("aim116", 50, 0));
             }
+
             @Override
             public DatabaseQuery<R> setParameter(String param, String value) {
               return null;
@@ -49,6 +53,57 @@ public class DatabaseManagerTest {
         return null;
       }
     };
-    assertEquals(session.createQuery("FROM User", User.class).list().get(0).getUsername(), "aim116");
+    assertEquals(session.createQuery("FROM User", User.class).list().get(0).getUsername(),
+        "aim116");
   }
+
+  @Test
+  public void queryingAllLocationsOpensSessionsAndBeginsTransaction() {
+    final DatabaseManager databaseManager = new DatabaseManager(sessionFactory);
+    beginTransactionBlock();
+    context.checking(new Expectations() {{
+      oneOf(session).createQuery("FROM User", User.class);
+    }});
+    closeTransactionBlock();
+    databaseManager.getAllLocations();
+  }
+
+  @Test
+  public void creatingTransactionBlockOpensSessionAndBeginsTransaction() {
+    context.checking(new Expectations() {{
+      oneOf(sessionFactory).openSession();
+      will(returnValue(session));
+      oneOf(session).beginTransaction();
+    }});
+    new TransactionBlock(sessionFactory);
+  }
+
+  @Test
+  public void closingTransactionBlockCommitsTransactionAndClosesSession() {
+    final TransactionBlock transactionBlock;
+    beginTransactionBlock();
+    context.checking(new Expectations() {{
+      oneOf(transaction).commit();
+      oneOf(session).close();
+    }});
+    transactionBlock = new TransactionBlock(sessionFactory);
+    transactionBlock.close();
+  }
+
+  private void beginTransactionBlock() {
+    context.checking(new Expectations() {{
+      ignoring(sessionFactory).openSession();
+      will(returnValue(session));
+      ignoring(session).beginTransaction();
+      will(returnValue(transaction));
+    }});
+  }
+
+  private void closeTransactionBlock() {
+    context.checking(new Expectations() {{
+      ignoring(transaction).commit();
+      ignoring(session).close();
+    }});
+  }
+
 }
