16d3d51d7227e7eb54bb07a6b0fb8af3aadc6544
Nik Vangerow <n@vangerow.uk>
2018-06-12 16:46:41 +0100

    one-liner DBTS queries

--- src/main/java/uk/avocado/notifications/DatabaseTokenStore.java
+++ src/main/java/uk/avocado/notifications/DatabaseTokenStore.java
@@ -28,28 +28,23 @@ public class DatabaseTokenStore implements TokenStore {
 
   private String getTokenForUser(String username) {
     try (final TransactionBlock tb = new TransactionBlock(sessionFactory)) {
-      final Optional<User> optional = tb.getSession()
+      return tb.getSession()
               .createQuery("FROM User WHERE username = :username", User.class)
               .setParameter("username", username)
               .setMaxResults(1).list().stream()
-              .findFirst();
-
-      return optional.map(User::getApsToken).orElse(null);
+              .findFirst().map(User::getApsToken).orElse(null);
     }
   }
 
   private void setTokenForUser(String username, String token) {
     try (final TransactionBlock tb = new TransactionBlock(sessionFactory)) {
-      final Optional<User> optional = tb.getSession()
-              .createQuery("FROM User WHERE username = :username", User.class)
+      tb.getSession().createQuery("FROM User WHERE username = :username", User.class)
               .setParameter("username", username)
               .setMaxResults(1).list().stream()
-              .findFirst();
-
-      optional.ifPresent(user -> {
-        user.setApsToken(token);
-        tb.getSession().saveOrUpdate(user);
-      });
+              .findFirst().ifPresent(user -> {
+                user.setApsToken(token);
+                tb.getSession().saveOrUpdate(user);
+              });
     }
   }
 }
