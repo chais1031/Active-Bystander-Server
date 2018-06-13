d170fc1e442f4a00d8bb824ff3309a3d33e852ee
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-13 20:46:16 +0100

    add quotation mark for the name of the table

---
+++ src/main/java/uk/avocado/model/HelpArea.java
@@ -0,0 +1,17 @@
+package uk.avocado.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "\"helpArea\"")
+public class HelpArea {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "helpAreaId")
+  private int helpAreaId;
+}
