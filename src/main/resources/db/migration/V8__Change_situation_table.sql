e84366f5804eda20b69161e60f69083554f5d5f9
Enyi Shang <enyi.shang16@imperial.ac.uk>
2018-06-13 23:49:07 +0100

    delete helparea and migration files

--- src/main/resources/db/migration/V8__Change_situation_table.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE situation DROP CONSTRAINT situation_pkey;
-ALTER TABLE situation RENAME COLUMN "id" TO "situation";
-ALTER TABLE situation ADD id SERIAL PRIMARY KEY;
--- src/main/resources/db/migration/V9__Add_help_area_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "helpArea" (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  helpAreaId INT NOT NULL
-);
